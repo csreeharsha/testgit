804acbbb5591c3aa4903509e7a8b0027660087b0
harsha <csreeharsha@gmail.com>
2016-09-12 17:12:55 +0530

    master and repo2

--- TestProject/src/main/java/com/sample/main/TestProject/App.java
+++ TestProject/src/main/java/com/sample/main/TestProject/App.java
@@ -9,11 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
         System.out.println( "Hello World2!" );
         System.out.println( "Hello World3!" );
-=======
         System.out.println("Added one more line");
->>>>>>> refs/remotes/origin/master
     }
 }
