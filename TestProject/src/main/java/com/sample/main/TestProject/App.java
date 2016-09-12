c2bb5706cbc46dc6a19162999d8daf2d2e95f627
Harsha <esakkiraj.a@noahdatatech.com>
2016-09-12 16:07:34 +0530

    TestProject

---
+++ TestProject/src/main/java/com/sample/main/TestProject/App.java
@@ -0,0 +1,14 @@
+package com.sample.main.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Added one more line");
+    }
+}
