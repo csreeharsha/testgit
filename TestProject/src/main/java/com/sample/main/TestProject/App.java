f7165c1598d84366c1a451dcba95a282d44d76c3
harsha <csreeharsha@gmail.com>
2016-09-12 16:43:56 +0530

    Intial Commit

---
+++ TestProject/src/main/java/com/sample/main/TestProject/App.java
@@ -0,0 +1,13 @@
+package com.sample.main.TestProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TestProject/src/main/java/com/sample/main/dao/MainApp.java
@@ -0,0 +1,13 @@
+package com.sample.main.dao;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Ahaa1");
+		System.out.println("Ahaa");
+		System.out.println("Ahaa2");
+
+	}
+
+}
